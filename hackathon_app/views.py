3f08424feb1d45c3136643522f962a78e4feffd6
wuayeq <quayew@janelia.hhmi.org>
2015-02-25 10:52:20 -0500

    testing debug

--- hackathon_app/views.py
+++ hackathon_app/views.py
@@ -1,9 +1,9 @@
 #debug flag. In production set debug="False"
 debug = True
-if debug == False:
-    from django.shortcuts import render
-    from django.utils import timezone
-    from django.template import RequestContext
+#if debug == False:
+ #   from django.shortcuts import render
+  #  from django.utils import timezone
+   # from django.template import RequestContext
 
 
 # imports
